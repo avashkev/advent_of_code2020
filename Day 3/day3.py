04f29c9666ba1e85699b2815dfcde9cf4c9fd4f9
Anastasia Vashkevich <anastasia@98point6.com>
2020-12-03 14:32:08 -0800

    fix 2 down tobbogan to start on the correct row

--- Day 3/day3.py
+++ Day 3/day3.py
@@ -2,9 +2,8 @@ text = open('input.txt', 'r')
 
 forest = text.readlines()
 
-
-
 slopes = [[1,1], [3,1], [5,1], [7,1], [1,2]]
+#slopes = [[1,2]]
 treeProduct = 1
 
 for slope in slopes:
@@ -15,36 +14,35 @@ for slope in slopes:
     treeCount = 0
 
     rowCount = 1
-    downCount = 1
+    downCounter = 0
 
     print("Slope: ",right,",",down)
     for line in forest:
         line = line.strip('\n')
     # print("Position: ", rowCount, ", ", pos)
 
-        if rowCount > 1 and downCount == down:
+        if rowCount > 1 and downCounter == down:
+            #print(rowCount, ", ", downCounter)
             if(line[pos] == '#'):
                 treeCount += 1
-             #   line = line[:pos] + 'X' + line[pos+1:]
-             #   print(line)
-             #   line = line[:pos] + '#' + line[pos+1:]
-            # print("Encountered Tree: ", line[pos])
-          #  else:
-               # line = line[:pos] + '0' + line[pos+1:]
-               # print(line)
-              #  line = line[:pos] + '.' + line[pos+1:]
-                # print("No Tree", line[pos])
+        #        line = line[:pos] + 'X' + line[pos+1:]
+        #        print(line)
+        #        line = line[:pos] + '#' + line[pos+1:]
+        #    else:
+        #        line = line[:pos] + '0' + line[pos+1:]
+        #        print(line)
+        #        line = line[:pos] + '.' + line[pos+1:]
 
             pos += right
             if pos >= len(line):
                 pos -= len(line)
-       # else:
-         #   print(line)
+    #    else:
+    #        print(line)
 
         rowCount +=1
-        downCount +=1
-        if downCount > down:
-            downCount = 1
+        downCounter +=1
+        if downCounter > down:
+            downCounter = 1
     print("Total Trees: ", treeCount, "\n")
     treeProduct *= treeCount
 
