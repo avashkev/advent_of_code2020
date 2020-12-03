63736c2746fd1130ed65444d1c60eb8121254601
Anastasia Vashkevich <anastasia@98point6.com>
2020-12-03 13:21:50 -0800

    day3 puzzle

---
+++ Day 3/day3.py
@@ -0,0 +1,50 @@
+text = open('test.txt', 'r')
+
+forest = text.readlines()
+
+
+
+slopes = [[1,1], [3,1], [5,1], [7,1], [1,2]]
+
+for slope in slopes:
+    right = slope[0]
+    down = slope[1]
+
+    pos = right
+    treeCount = 0
+
+    rowCount = 1
+    downCount = 1
+
+    print("Slope: ",right,",",down)
+    for line in forest:
+        line = line.strip('\n')
+    # print("Position: ", rowCount, ", ", pos)
+
+        if rowCount > 1 and downCount == down:
+            if(line[pos] == '#'):
+                treeCount += 1
+                line = line[:pos] + 'X' + line[pos+1:]
+                print(line)
+                line = line[:pos] + '#' + line[pos+1:]
+            # print("Encountered Tree: ", line[pos])
+            else:
+                line = line[:pos] + '0' + line[pos+1:]
+                print(line)
+                line = line[:pos] + '.' + line[pos+1:]
+                # print("No Tree", line[pos])
+
+            pos += right
+            if pos >= len(line):
+                pos -= len(line)
+        else:
+            print(line)
+
+        rowCount +=1
+        downCount +=1
+        if downCount > down:
+            downCount = 1
+
+    print("Total Trees: ", treeCount, "\n")
+
+text.close()
